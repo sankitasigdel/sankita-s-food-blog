cedf2aba2ab4df7bdd8a95bd7cb23a0ef6ab29bd
Sankita Sigdel <sankitasigdel@Sankitas-MacBook-Air.local>
2023-03-15 15:10:58 +0545

    first commit

---
+++ myRecipes.js
@@ -0,0 +1,44 @@
+const myRecipes = [
+    {
+      title: "Spinach and Feta Stuffed Chicken Breast",
+      date: "2023-03-15",
+      image: "https://wallpapershome.com/images/pages/pic_h/16604.jpg",
+      author: "John Smith",
+      content: "This delicious and healthy recipe features tender chicken breasts stuffed with a savory mixture of spinach and feta cheese. To prepare, butterfly the chicken breasts and season them with salt and pepper. In a separate bowl, mix together chopped spinach, crumbled feta cheese, garlic, and olive oil. Spread the spinach mixture evenly over each chicken breast, then fold the chicken back up and secure with toothpicks. Bake the chicken in the oven until cooked through and golden brown, then serve hot with your favorite side dishes. Enjoy!"
+    },
+    {
+      title: "Slow Cooker Beef Stew",
+      date: "2023-03-14",
+      image: "https://wallpapershome.com/images/pages/pic_h/16604.jpg",
+      author: "Jane Doe",
+      content: "This hearty and comforting beef stew is perfect for a cold winter day. To prepare, brown beef chuck roast in a skillet, then transfer to a slow cooker along with carrots, potatoes, onion, garlic, beef broth, and seasonings. Cook on low for 8 hours, then add frozen peas and cook for an additional 15-20 minutes. Serve hot with crusty bread and enjoy!"
+    },
+    {
+      title: "Mushroom Risotto",
+      date: "2023-03-13",
+      image: "https://wallpapershome.com/images/pages/pic_h/16604.jpg",
+      author: "John Smith",
+      content: "This rich and creamy mushroom risotto is a perfect comfort food. To prepare, sauté chopped onions and garlic in olive oil, then add sliced mushrooms and cook until browned. Stir in Arborio rice and cook for a few minutes, then add white wine and cook until absorbed. Gradually add hot chicken broth, stirring constantly, until the rice is tender and creamy. Finish with grated Parmesan cheese and chopped parsley. Serve hot and enjoy!"
+    },
+    {
+      title: "Grilled Salmon with Lemon and Dill",
+      date: "2023-03-12",
+      image: "https://wallpapershome.com/images/pages/pic_h/16604.jpg",
+      author: "Jane Doe",
+      content: "This simple and delicious grilled salmon recipe is perfect for a summer barbecue. To prepare, season salmon fillets with salt, pepper, and lemon juice, then grill over medium-high heat for about 5-6 minutes per side, until cooked through. Top with chopped fresh dill and serve hot with grilled vegetables or a salad. Enjoy!"
+    },
+    {
+      title: "Roasted Vegetable Quinoa Salad",
+      date: "2023-03-11",
+      image: "https://wallpapershome.com/images/pages/pic_h/16604.jpg",
+      author: "John Smith",
+      content: "This healthy and flavorful quinoa salad is perfect for lunch or dinner. To prepare, roast chopped vegetables such as bell peppers, zucchini, and onion with olive oil and seasonings. Cook quinoa separately and mix with the roasted vegetables, chopped fresh herbs, and a lemon vinaigrette. Serve at room temperature or chilled and enjoy!"
+    },
+    {
+      title: "Roasted Vegetable Quinoa Salad",
+      date: "2023-03-11",
+      image: "https://wallpapershome.com/images/pages/pic_h/16604.jpg",
+      author: "John Smith",
+      content: "This healthy and flavorful quinoa salad is perfect for lunch or dinner. To prepare, roast chopped vegetables such as bell peppers, zucchini, and onion with olive oil and seasonings. Cook quinoa separately and mix with the roasted vegetables, chopped fresh herbs, and a lemon vinaigrette. Serve at room temperature or chilled and enjoy!"
+    }
+];
